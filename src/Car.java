c2d6a89dac6286a393c28f6dc1cf56960ab9603d
OlegDrozdov <dr-oleg-103@bk.ru>
2022-05-02 12:46:12 +0300

    first commit

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    public void build(){
+        System.out.println("You must build a new car");
+    }
+    public void buy(){
+        System.out.println("I will buy this car");
+    }
+    public void drozd(){
+        System.out.println("Oleg kupit sebe tachky! ");
+    }
+    int carSpeed = 0;
+
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        //write your code here
+        Car newCar = new Car();
+        newCar.build();
+        newCar.buy();
+        newCar.drozd();
+        char num1 = 'd';
+        int result = num1 + 3;
+        System.out.println("Result " + result);
+    }
+
+}
